9b6c19f3519e54ebe5c862993ade7f7dc11af5cc
zscholts <zachscholts@quickenloans.com>
2020-09-01 15:05:38 -0400

    initial commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "SWEET SWEET COFFEE!!!!!";
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult ThankYou(string firstname, string lastname, string email, string password)
+        {
+            ViewBag.Welcome = $"Welcome, {firstname} {lastname}!!";
+            string userEmail = email;
+            string userPassword = password;
+            return View();
+
+        }
+
+    }
+}
